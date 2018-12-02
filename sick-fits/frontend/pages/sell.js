947a668f8a7484f7b6bf0840abcc033a746a0b0c
CJ Kenney <ckenney422@gmail.com>
2018-12-02 13:33:10 -0600

    nextjs routing init

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Home = props => (
+    <div>
+        <p>Sell Page</p>
+        <Link href="/">
+            <a>Home</a>
+        </Link>
+    </div>
+)
+
+export default Home;
