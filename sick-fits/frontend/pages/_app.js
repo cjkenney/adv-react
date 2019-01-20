288d584b5a75a45d1e6e9bf71409e0b1172ffd03
CJ Kenney <ckenney422@gmail.com>
2019-01-20 16:50:06 -0600

    fix spacing

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -6,7 +6,7 @@ import withData from '../lib/withData';
 class MyApp extends App {
     static async getInitialProps({ Component, ctx }) {
         let pageProps = {};
-        if(Component.getInitialProps) {
+        if (Component.getInitialProps) {
             pageProps = await Component.getInitialProps(ctx);
         }
         // this exposes the query to the user
