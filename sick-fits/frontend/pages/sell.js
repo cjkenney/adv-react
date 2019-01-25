cf75e01364a6b2b3c5b5c3be3e358ce19ef1e5dc
CJ Kenney <ckenney422@gmail.com>
2019-01-24 19:53:34 -0600

    create CreateItem

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,8 +1,10 @@
+import CreateItem from '../components/CreateItem';
 import Link from 'next/link';
 
 const Home = props => (
     <div>
         <p>Sell Page</p>
+        <CreateItem />
     </div>
 )
 
