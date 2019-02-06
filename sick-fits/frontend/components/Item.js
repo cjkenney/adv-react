a26ef6db7a7d22eec8ad360cb8d8ae34201cd8f0
CJ Kenney <ckenney422@gmail.com>
2019-02-05 22:10:47 -0600

    deleteItems

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -4,39 +4,44 @@ import Link from 'next/link';
 import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
+import DeleteItem from './DeleteItem';
 import formatMoney from './../lib/formatMoney';
 
 export default class Item extends Component {
-    static propTypes = {
-        item: PropTypes.object.isRequired,
-    };
+  static propTypes = {
+    item: PropTypes.object.isRequired
+  };
 
-    render() {
-        const { item } = this.props;
-        return (
-            <ItemStyles>
-                <Title>
-                    {item.image && <img src={item.image} alt={item.title} />}
-                    <Link href={{
-                        pathname: '/item',
-                        query: { id: item.id },
-                    }}>
-                        <a>{item.title}</a>
-                    </Link>
-                </Title>
-                <PriceTag>{formatMoney(item.price)}</PriceTag>
-                <p>{item.description}</p>
-                <div className="buttonList">
-                    <Link href={{
-                        pathname: 'update',
-                        query: { id: item.id },
-                    }}>
-                        <a>Edit ✏️</a>
-                    </Link>
-                    <button>Add To Cart</button>
-                    <button>Delete</button>
-                </div>
-            </ItemStyles>
-        );
-    }
+  render() {
+    const { item } = this.props;
+    return (
+      <ItemStyles>
+        <Title>
+          {item.image && <img src={item.image} alt={item.title} />}
+          <Link
+            href={{
+              pathname: '/item',
+              query: { id: item.id }
+            }}
+          >
+            <a>{item.title}</a>
+          </Link>
+        </Title>
+        <PriceTag>{formatMoney(item.price)}</PriceTag>
+        <p>{item.description}</p>
+        <div className="buttonList">
+          <Link
+            href={{
+              pathname: 'update',
+              query: { id: item.id }
+            }}
+          >
+            <a>Edit ✏️</a>
+          </Link>
+          <button>Add To Cart</button>
+          <DeleteItem id={item.id}>Delete This Item</DeleteItem>
+        </div>
+      </ItemStyles>
+    );
+  }
 }
