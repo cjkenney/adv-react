29b392f94d0d35827ef3d4c5bf7e5948dd47bec7
CJ Kenney <ckenney422@gmail.com>
2019-02-06 21:23:33 -0600

    finish user signup flow backend

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -35,6 +38,28 @@ const Mutations = {
     // TODO
     // 3. Delete it
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    // lowercase user's email
+    args.email = args.email.toLowerCase();
+    // hash password
+    const password = await bcrypt.hash(args.password, 10);
+    // create user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: { ...args, password, permissions: { set: ['USER'] } }
+      },
+      info
+    );
+    // create the JWT token for the user
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // we set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+    });
+    // return the user to the browser
+    return user;
   }
 };
 
