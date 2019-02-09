791065fec38f9ed96b31f763230393c1f3da06e9
CJ Kenney <ckenney422@gmail.com>
2019-02-09 16:03:15 -0600

    add currently logged in user function

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -7,6 +8,17 @@ const server = createServer();
 
 // parse JWT to authenticate current user
 server.express.use(cookieParser());
+
+// decode the JWT so we can get the user ID on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userID onto the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 // TODO Use express middleware to populate current user
 
 server.start(
